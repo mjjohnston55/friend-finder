3a09e875d25e88c57846f7a65864c3a10a4d5e0c
Michael Johnston <mjjohnston55@gmail.com>
2019-11-04 01:04:30 -0800

    skeleton of api routes

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,15 @@
+
+
+
+module.exports = function(app) {
+  
+    // GET route for displaying JSON of all possible friends
+  app.get("/api/friends/", function(req, res) {
+
+  });
+
+//   POST route for incoming survey results and compatibility logic handling
+  app.post("/api/posts", function(req, res) {
+  });
+
+}
